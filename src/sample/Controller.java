612baa26dda161d5a72c28b5f415fc9caf9707e4
Sergi <sergibca@gmail.com>
2016-01-14 19:47:45 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,29 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    public TextField field1;
+    public TextField field2;
+    public TextField field3;
+    public TextField field4;
+    public TextField field5;
+    public TextField field6;
+
+    // Añadir libros
+
+    public void anyadirLibro(ActionEvent actionEvent) {
+
+    }
+
+    public void anyadirSocio(ActionEvent actionEvent) {
+
+    }
+
+    public void anyadirPrestamo(ActionEvent actionEvent) {
+
+
+    }
+}
